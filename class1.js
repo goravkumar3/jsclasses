33249a74a22956becbda088eb1cbff10cb259de5
[gorav kumar] <[kg558390@gmail.com]>
2023-09-01 19:42:52 +0500

    JsClass

---
+++ class1.js
@@ -0,0 +1,15 @@
+var a=20
+var s="Hello World"
+var b=true;
+console.log(a)
+console.log(s)
+console.log(b)
+var c=3;
+console.log("Addition : "+(a+c))
+console.log("Subtract : "+(a-c))
+console.log("MUltiplication : "+a*c)
+console.log("Divide : "+a/c)
+console.log("Module : "+a%c)
+
+var p=prompt("What is your name?")
+alert("Welcome "+p)
