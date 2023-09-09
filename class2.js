9797ce7e3c2c26ca611a3f71a405bbeefde09ce7
[gorav kumar] <[kg558390@gmail.com]>
2023-09-09 19:16:48 +0500

    classesupdate

--- class2.js
+++ class2.js
@@ -1,9 +1,32 @@
-var num=prompt("Enter any number")
-var sign=prompt("Enter arthematic operator +,-,*,/");
-var num1=prompt("Enter any number");
-if(sign==="+"){
-    alert(num+sign+num1+"="+(+num+ +num1))
+// var num=prompt("Enter any number")
+// var sign=prompt("Enter arthematic operator +,-,*,/");
+// var num1=prompt("Enter any number");
+// if(sign==="+"){
+//     alert(num+sign+num1+"="+(+num+ +num1))
+// }
+// else if(sign==="-"){
+//     alert(num+sign+num1+"="+(num-num1))
+// }
+// else if(sign==="*"){
+//     alert(num+sign+num1+"="+(num*num1))
+// }
+// else if(sign==="/"){
+//     alert(num+sign+num1+"="+(num/num1))
+// }
+// else{
+//     alert("you enter wrong sign")
+// }
+var city=prompt("Enter your city name")
+city=city.toLowerCase()
+if(city==="hyderabad"||city==="karachi"){
+    var age=prompt("Enter your age")
+    if(age>18&&age<40){
+        alert("You can apply for job")
+    }
+    else{
+        alert("you can't apply for job")
+    }
 }
 else{
-    alert("you enter wrong sign")
+    alert("you can't apply for job")
 }
---
+++ class3.js
@@ -0,0 +1,23 @@
+var a=6;
+console.log(a++)
+console.log(a++)  
+console.log(a++)  
+console.log(a++)
+console.log("Pre-increament")  
+var b=6;
+console.log(++b)
+console.log(++b)  
+console.log(++b)  
+console.log(++b)  
+console.log("start decrement")
+var c=6;
+console.log(c--)
+console.log(c--)  
+console.log(c--)  
+console.log(c--)
+console.log("Pre-decreament")  
+var d=6;
+console.log(--d)
+console.log(--d)  
+console.log(--d)  
+console.log(--d) 
