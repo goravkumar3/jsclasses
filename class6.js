96f18220740358577d11357f0e8de5bb318e9669
[gorav kumar] <[kg558390@gmail.com]>
2023-09-21 19:36:04 +0500

    update

---
+++ class6.js
@@ -0,0 +1,17 @@
+var arr=[2,3,4,5,6,7,8]
+console.log(arr)
+console.log(arr[6])
+arr.pop()
+console.log(arr)
+arr.push(10)
+console.log(arr)
+arr.shift()
+console.log(arr)
+arr.unshift(23)
+console.log(arr)
+arr.splice(1,3,23,34,54)
+console.log(arr)
+var arr1=arr.slice(2,5)
+console.log(arr1)
+
+
