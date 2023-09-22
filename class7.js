f3da240736698023e3cd98b440b02e7c5cf1a1b6
[gorav kumar] <[kg558390@gmail.com]>
2023-09-22 19:27:05 +0500

    update

---
+++ class7.js
@@ -0,0 +1,24 @@
+
+do {
+    var a = prompt("Enter first number");
+var s = prompt("Enter operator +,-,*,/");
+var b = prompt("Enter second number");
+    switch (s) {
+        case "+":
+          alert(a + s + b + "=" + (+a + +b));
+          break;
+        case "-":
+          alert(a + s + b + "=" + (a - b));
+          break;
+        case "*":
+          alert(a + s + b + "=" + a * b);
+          break;
+        case "/":
+          alert(a + s + b + "=" + a / b);
+          break;
+        default:
+          alert("You entered wrong operator")
+      }
+      var ask=prompt("Do you want do again?")
+      ask=ask.toLowerCase()
+} while (ask==="yes");
